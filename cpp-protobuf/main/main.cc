bd68835748d099eee2a158954c1c6a217707d99b
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2022-02-10 22:42:25 +0100

    clean up a bit !

--- cpp-protobuf/main/main.cc
+++ cpp-protobuf/main/main.cc
@@ -1,4 +1,3 @@
-#include <sys/socket.h>
 #include <sstream>
 #include <utility>
 #include <inttypes.h>
@@ -8,7 +7,6 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "test.pb.h"
-#include "nvs_flash.h"
 
 static const char* TAG = "Main";
 
@@ -18,12 +16,7 @@ extern "C" {
 
 void app_main(void) 
 {
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+
 
     int32_t id = 0;
     while(1)
